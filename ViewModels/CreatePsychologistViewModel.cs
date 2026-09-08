f56accfb8492231c50f221c2f33f87a0a455ee07
zafawniar <zafawniar@gmail.com>
2026-09-08 17:41:02 +0700

    feat: create view model for new psychologist account creation

---
+++ ViewModels/CreatePsychologistViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SangtuariCareerCompass.ViewModels
+{
+    public class CreatePsychologistViewModel
+    {
+        [Required(ErrorMessage = "Nama Lengkap wajib diisi.")]
+        [MaxLength(100)]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email wajib diisi.")]
+        [EmailAddress(ErrorMessage = "Format email tidak valid.")]
+        [MaxLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Role wajib dipilih.")]
+        public string Role { get; set; } = "Staff"; // "Head" atau "Staff"
+    }
+}
